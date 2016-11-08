03bf8c98acc67de0b690ca9880fdad9b181650f6
Joel <joeltwaage@gmail.com>
2016-11-08 15:52:36 -0800

    clean up some of the faker generation code in mirage/factories/account.js

--- mirage/config.js
+++ mirage/config.js
@@ -1,9 +1,7 @@
 export default function() {
   this.namespace = 'api';
 
-  this.get('/accounts', (schema, request) +> {
-    return schema.accounts.all();
-  });
+  this.get('/accounts');
 
 
   this.get('/accounts/:id');
--- mirage/factories/account.js
+++ mirage/factories/account.js
@@ -2,7 +2,7 @@ import { Factory, faker } from 'ember-cli-mirage';
 
 
 export default Factory.extend({
-  name(i) { return `Name ${i+1}` },
+  name(i) { return (faker.name.firstName() + ' ' + faker.name.lastName() + ': ' + (i+1)); },
   contact() {
     return (faker.name.firstName() + ' ' + faker.name.lastName());
   },
