f132fd74bf6ec95c334449f361a3f425d3048487
Joel <joeltwaage@gmail.com>
2016-11-08 16:07:21 -0800

    adjust order of Index page, add faker email

--- mirage/factories/account.js
+++ mirage/factories/account.js
@@ -3,8 +3,6 @@ import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
   name(i) { return (faker.name.firstName() + ' ' + faker.name.lastName() + ': ' + (i+1)); },
-  contact() {
-    return (faker.name.firstName() + ' ' + faker.name.lastName());
-  },
-  email: "test@test.test",
+  contact() { return (faker.name.firstName() + ' ' + faker.name.lastName()); },
+  email() { return faker.internet.email(); }
 });
