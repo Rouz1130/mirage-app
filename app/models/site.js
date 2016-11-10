82adf7e8892dcfbb2128243af706322136bdc934
Joel <joeltwaage@gmail.com>
2016-11-10 11:52:34 -0800

    working on model relationships, commiting to sync and prevent conflicts

--- app/models/site.js
+++ app/models/site.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   siteName: DS.attr(),
-  account: DS.attr(),
+  account: DS.belongsTo('account'),
   timeZone: DS.attr(),
   maxCharge: DS.attr(),
   siteId: DS.attr(),
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,8 +1,10 @@
 export default function(server) {
 
+  let account = server.create('account');
+
   server.createList('account', 10);
   server.createList('location', 10);
-  server.createList('site', 8);
+  server.createList('site', {account: account.id});
   server.createList('asset', 3);
   server.createList('route', 5);
 }
