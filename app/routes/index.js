27940461407e82cc3e6770b147e6cb7c9d4feac5
Joel <joeltwaage@gmail.com>
2016-11-09 16:42:32 -0800

    working of getting the indivdual site-tiles to display

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,3 @@
-// import Ember from 'ember';
-//
-// const { RSVP, $ } = Ember;
-//
-// export default Ember.Route.extend({
-//   model() {
-//     return RSVP.hash({
-//       accounts: $.getJSON('/api/accounts'),
-//       account2: $.getJSON('/api/accounts/2'),
-//     });
-//   }
-// });
-
-// import Ember from 'ember';
-//
-// export default Ember.Route.extend({
-//   model() {
-//     return this.store.findAll('account');
-//   }
-// });
-
 
 import Ember from 'ember';
 
