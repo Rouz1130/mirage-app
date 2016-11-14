9383166710bf999cdf5c919f518117d93273effe
Rick Thornbrugh <Rick Thornbrugh>
2016-11-14 10:36:26 -0800

    revise accouts page

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ export default Ember.Route.extend({
   model() {
     return RSVP.hash({
       accounts: $.getJSON('/api/accounts'),
-      account2: $.getJSON('/api/accounts/2'),
+      
     });
     }
 });
